e886e199e3868f5a050b1bef2ceb468b41fd89a3
Pardeep Pathania <pardeeppathania09@gmail.com>
2025-05-31 11:55:25 -0400

    UI changes

--- photo_selector/utils.py
+++ photo_selector/utils.py
@@ -3,10 +3,10 @@ from flask import make_response, request, current_app
 
 def auth_required(f):
     @wraps(f)
-    def decorarted(*args, **kwargs):
+    def decorated(*args, **kwargs):
         auth = request.authorization
-        if auth and auth.username == current_app.config["USER"] and auth.password == current_app.config["PASS"]:
+        if auth and auth.username == current_app.config["USER"] and auth.password == str(current_app.config["PASS"]):
             return f(*args, **kwargs)
         return make_response("<h1>Access Denied</h1>", 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
     
-    return decorarted
+    return decorated
